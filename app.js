a39ce7ab5301d530b4b70c92f7d8816ee23cbb13
powolnymarcel <powolnymarcel@gmail.com>
2015-10-25 15:21:49 +0100

    essai avec pluralize

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var app = angular.module('basicRedditClone', []);
 app.controller('ControlleurPrincipal', ['$scope',function($scope){
 
         $scope.posts = [
-            'Post 1',
-            'Post 2',
-            'Post 3',
-            'Post 4',
-            'Post 5',
-            'Post 6',
+            {"titre": 'post 1', "votesPositifs": 5},
+            {"titre": 'post 2', "votesPositifs": 2},
+            {"titre": 'post 3', "votesPositifs": 15},
+            {"titre": 'post 4', "votesPositifs": 9},
+            {"titre": 'post 5', "votesPositifs": 4},
+            {"titre": 'post 5', "votesPositifs": 0},
+            {"titre": 'post 5', "votesPositifs": 1}
         ];
     }]);
+
